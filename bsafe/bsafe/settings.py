3180ea1a40b284ce3cc8e55e9fbd0add1e3a7887
Efthymios1994 <efthimios.efthimiadis@infoteam-software.gr>
2024-11-19 11:25:20 +0200

    stage for render hosting

--- bsafe/bsafe/settings.py
+++ bsafe/bsafe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(m#=1v-zp4n61o3!awo+9*!wxfnvz=myg9nw4eq9_60vc6k=#9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -97,7 +98,7 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-DATABASES["default"] = dj_database_url.parse("postgresql://bsafe_user:ytQgP0A0TF1AlZLFnkaDi6G6CFwG6lqN@dpg-csu42qa3esus73c6u340-a.oregon-postgres.render.com/bsafe")
+DATABASES["default"] = dj_database_url.parse(config("DATABSE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
